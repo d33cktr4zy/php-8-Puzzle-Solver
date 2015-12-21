f63bb31adce20b554a78d2321c429b2204d6984b
d33cktr4zy <gabriel.fermy@gmail.com>
2015-12-21 12:59:34 +0700

    Nothing

---
+++ Kendaraan.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gabriel
+ * Date: 12/16/2015
+ * Time: 09:14 AM
+ */
+
+namespace App\Puzzle;
+
+
+class Kendaraan
+{
+    public $alatKendali;
+    public $kapasitas;
+
+    public function maju($jarak){
+
+    }
+
+    public function mundur
+}
